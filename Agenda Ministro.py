03e813ff66a1f814f8cfcbe9c6d3e50147e5b98d
Victor Hugo <hugovco511@gmail.com>
2024-05-30 10:22:09 -0300

    eventos do ministro

--- Agenda Ministro.py
+++ Agenda Ministro.py
@@ -1,26 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
+import html
+import unicodedata
+import json
 
 def obter_agenda_ministro(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+    
+    eventos = soup.find('div', attrs={'ng-init': re.compile(r'^events')})
 
-    eventos = soup.find_all('div', class_='evento')
+    padrao = r'\[(.*?)\]'
+    eventos = re.findall(padrao, str(eventos))
+    eventos = html.unescape(eventos[0])
+    eventos = unicodedata.normalize('NFC', eventos)
+
+    with open('pagina.html', 'w', encoding='utf-8') as file:
+        file.write(str(eventos))
+
+#     dados = []
+#     for evento in eventos:
+#         data = evento.get('data_date')
+#         hora = evento.find('div', class_='fc-event-time').text.strip()
+#         descricao = evento.find('div', class_='fc-event-title').text.strip()
+#         dados.append({"Data": data, "Hora": hora, "Evento": descricao})
     
-    dados = []
-    for evento in eventos:
-        data = evento.find('span', class_='data').text.strip()
-        hora = evento.find('span', class_='hora').text.strip()
-        descricao = evento.find('span', class_='descricao').text.strip()
-        dados.append({"Data": data, "Hora": hora, "Evento": descricao})
-    
-    return pd.DataFrame(dados)
+#     return pd.DataFrame(dados)
 
 url_ministro = "https://eagendas.cgu.gov.br/?_token=2LL4g4Z10hKNCjonNfqsfcxcv8bmygmyWNMADkBI&filtro_orgaos_ativos=on&filtro_orgao=1384&filtro_cargos_ativos=on&filtro_cargo=MINISTRO%28A%29+DA+FAZENDA&filtro_apos_ativos=on&filtro_servidor=14641&cargo_confianca_id=&is_cargo_vago=false#divcalendar"
+obter_agenda_ministro(url_ministro)   
 
-df_ministro = obter_agenda_ministro(url_ministro)
+# df_ministro = obter_agenda_ministro(url_ministro)
 
-df_ministro.to_excel('agenda_ministro_fazenda.xlsx', index=False)
+# df_ministro.to_excel('agenda_ministro_fazenda.xlsx', index=False)
 
-print("Agenda do Ministro da Fazenda salva em 'agenda_ministro_fazenda.xlsx'")
+# print("Agenda do Ministro da Fazenda salva em 'agenda_ministro_fazenda.xlsx'")
